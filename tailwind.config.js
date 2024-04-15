d0a6798a0116a4d3754b5945241fcea41cd6f25f
sivanraj <sivanraj86@gmail.com>
2024-04-15 14:35:37 +0530

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        RBlue: "#003E9C",
+        RText: "#003E9C",
+        RButton: "#96C0FF",
+        OBlue: "#003E9C",
+        Side: "#EFEFEF",
+        Otpcolor: "#DAE2F5",
+      },
+    },
+  },
+  plugins: [],
+};
